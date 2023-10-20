ca73922034ac850ee36ebdc62ecad92048635202
SahinS9 <shahinsaatov@gmail.com>
2023-10-20 23:46:23 +0400

    project files, folder created

--- app.py
+++ app.py
@@ -1,37 +1,19 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-from datetime import datetime, date
-
+from flask import render_template
+from views import *
+from models import User, Task
 
 app = Flask(__name__)
+
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
 app.config['SQLALCHEMY_TRACK_MODIFICATION'] = False
-
 
 db = SQLAlchemy(app)
 app.app_context().push()
 
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key = True)
-    name = db.Column(db.String(50))
-    email = db.Column(db.String(100), unique = True)
-    date_joined = db.Column(db.Date, default = datetime.utcnow)
-    tasks =  db.relationship('Task', backref = 'user', cascade='save-update, delete-orphan' )
+@app.route('/')
+def index():
+    return render_template('home.html')
 
-
-    def __repr__(self):
-        return f"{self.name} with {self.email} at {self.date_joined} as {self.id}"
-
-
-class Task(db.Model):
-    id = db.Column(db.Integer, primary_key = True)
-    name = db.Column(db.String(50))
-    description = db.Column(db.String(100))
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-
-    def __repr__(self):
-    if self.user:
-        return f"{self.user.name}'s task: {self.name}"
-    else:
-        return f"Task with no associated user: {self.name}"
-
+if __name__ == '__main__':
+    app.run(debug = True)
---
+++ views.py
@@ -0,0 +1,5 @@
+# views.py
+from flask import render_template
+
+def home():
+    return render_template('home.html')
